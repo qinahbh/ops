81b2ff876c4365b048473948a1eb0fa47aa123f6
qinahbh <qinahbh@gmail.com>
2017-07-28 20:31:09 +0800

    add about branch

--- git.sh
+++ git.sh
@@ -24,4 +24,27 @@ essential step
 #remote add repo after graph
 git remote add origin https://www.github.com/qinahbh/ops.git
 git push -u origin master
-
+#fork repo(for privarite)
+graphy fork self branch with ssh: git@github.com:coco/user.git
+git clone git@github.com:coco:/user.git user
+#pull repo(with fetch and merge)
+git pull git://github.com/tom/test.git
+#new version
+git branch bug_fix
+#insert into bug_fix
+git checkout bug_fix
+git checkout master
+#merge new branch
+git merge bug_fix
+#check branch
+git branch -r
+#rename branch
+git checkout -b bug_fix_local bug_fix
+#check all commit msn
+git log
+#check modified latest
+git diff
+#check diff in some times
+git show
+#show current work status
+git status
